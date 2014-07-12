97a385a2e3d14d59c11dd58171ab766454842fbd
Vladimir Rybakov <vladimir.rybakov@skype.net>
2014-07-13 00:42:34 +0400

    create order based on processing plan

--- atrspb/atrspb.js
+++ atrspb/atrspb.js
@@ -19,7 +19,57 @@ function init() {
     for(i = 0, l = plans.length; i < l; i += 1) {
       plan = plans[i];
       $vm.processingPlans.push({
-        name: plan.name
+        name: plan.name,
+        data: plan
+      });
+    }
+  }
+
+  function onNewCustomOrder() {
+    $log('onNewCustomOrder', $vm.selectedPlan());
+    var i,
+        materials = $vm.selectedPlan().data.material,
+        posinionsQuantity = materials.length,
+        uuid,
+        quantities = {},
+        positions = []
+
+    for(i = 0; i < posinionsQuantity; i++) {
+      uuid = materials[i].goodUuid;
+      quantities[uuid] = materials[i].quantity
+      $client.load('Good', uuid, function(dummy, good){
+        positions.push({
+          vat: 18,
+          goodUuid: good.uuid,
+          quantity: quantities[good.uuid],
+          discount: 0,
+          reserve: 0,
+          basePrice: {
+            sum: good.salePrice,
+            sumInCurrency: good.salePrice
+          }
+        });
+
+        if(positions.length === posinionsQuantity) {
+          console.log(positions);
+
+          var order = {
+            vatIncluded: true,
+            applicable: true,
+            sourceStoreUuid: "123f239f-0216-11e4-4d38-002590a28eca",
+            payerVat: true,
+            sourceAgentUuid: "123fbe60-0216-11e4-4cbf-002590a28eca",
+            targetAgentUuid: "123da6d2-0216-11e4-3a51-002590a28eca",
+            moment: new Date(),
+            name: new Date().getTime().toString(),
+            customerOrderPosition: positions
+          }
+
+          $client.save("moysklad.customerOrder", order, function(dummy, order){
+            location.hash = '#customerorder/edit?id=' + order.uuid;
+          });
+        }
+
       });
     }
   }
@@ -27,9 +77,9 @@ function init() {
   function onCustomerOrder() {
 
     $log('onCustomerOrder');
-    $api.wait.elementRender('.pump-title-panel', function(){
+    $api.wait.elementRender('.pump-title-panel:visible', function(){
 
-      var container = $('>tbody', '.pump-title-panel'),
+      var container = $('>tbody', '.pump-title-panel:visible'),
           td = $('.taist_container', container),
           tr,
           originalButton;
@@ -47,16 +97,11 @@ function init() {
         originalButton
           .clone()
           .appendTo(tr)
-          .click(function(){
-            location.hash = '#customerorder/edit?new';
-            originalButton.click();
-            $log('onClick', $vm.selectedPlan());
-          });
-
+          .click(onNewCustomOrder);
       }
 
-      processingPlan = $client.from('ProcessingPlan').count(1000).load();
-      parseProcessingPlans(processingPlan);
+      var processingPlans = $client.from('ProcessingPlan').load();
+      parseProcessingPlans(processingPlans);
 
       $('#taist_processingPlans').appendTo(td);
     });
@@ -84,6 +129,14 @@ function init() {
 
       $client = require('moysklad-client').createClient();
 
+      //Fixed bug with moysklad-client
+      require('xmldom').DOMImplementation = function(){
+        this.createDocument = function(){
+          return document.implementation.createDocument('', '', null);
+        }
+        return this;
+      }
+
       $div = $('<div>')
         .css({display: 'none'})
         .prependTo('body');
@@ -108,7 +161,7 @@ function init() {
 
       ko.applyBindings($vm);
 
-      $api.hash.when(/^.customerorder/, onCustomerOrder);
+      $api.hash.when(/^#customerorder/, onCustomerOrder);
     });
   }
 
