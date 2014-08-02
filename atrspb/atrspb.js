6c42ae34d883cd5198abad8d444ce6d9056a25df
Vladimir Rybakov <vladimir.rybakov@skype.net>
2014-08-02 15:32:14 +0400

    added position to the current order

--- atrspb/atrspb.js
+++ atrspb/atrspb.js
@@ -536,7 +536,8 @@ function init() {
 
       'CommonService.getItemTO': function(requestData, responseText){
         $log(requestData, responseText);
-        if($app.getLastState().name !== STATE.ORDER.newGoodWaited)
+        var state = $app.getLastState();
+        if(state && state.name !== STATE.ORDER.newGoodWaited)
         {
           return false;
         }
@@ -553,11 +554,47 @@ function init() {
       'OrderService.stockForConsignmentsWithReserve': function(requestData, responseText){
         $log(requestData, responseText);
         var state = $app.getLastState();
-        if(state.name !== STATE.ORDER.newGoodSelected) {
+        if(state && state.name !== STATE.ORDER.newGoodSelected) {
           return false;
         }
 
         $log('New position', state.data);
+        $client.load('Good', state.data.uuid, function(dummy, good){
+
+          if(!$vm.goods[good.uuid]) {
+            $vm.goods[good.uuid] = {
+              name: ko.observable(good.name)
+            };
+          }
+
+          var position = createCustomerOrderPosition({
+            data: {
+              vat: 18,
+              goodUuid: good.uuid,
+              quantity: 1,
+              discount: 0,
+              reserve: 0,
+              basePrice: {
+                sum: good.salePrice,
+                sumInCurrency: good.salePrice
+              },
+              price: {
+                sum: good.salePrice,
+                sumInCurrency: good.salePrice
+              },
+            }
+          });
+
+          var order = $vm.selectedOrder();
+          position._quantity = ko.computed(function(){
+            var quantity = this._quantityPerPresent() * order._presentsCount();
+            this.quantity(quantity);
+            return quantity;
+          }, position);
+
+          $log(position);
+          order.customerOrderPosition.push(position);
+        });
       },
 
     });
